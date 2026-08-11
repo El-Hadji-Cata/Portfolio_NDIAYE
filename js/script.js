7c01e33f7066ca96354b64558d69de18965fb72f
El Hadji Cata NDIAYE <paomar8@gmail.com>
2026-08-11 16:48:41 +0200

    modif titre & a propos, ajout de realisations, experiences et contact

--- js/script.js
+++ js/script.js
@@ -1,10 +1,21 @@
-const openMenu = ()=> {
+const openMenu = () => {
     const menu = document.querySelector('.header-menu');
-    menu.classList.toggle('visible')
-    if(menu.classList.contains('visible')){
-        document.querySelector('header .material-icons').innerHTML = "close";
-    }else {
-        document.querySelector('header .material-icons').innerHTML = "list_alt"
-    }
+    menu.classList.toggle('visible');
 
-}
+    const icon = document.querySelector('.header-menu-mobile .material-icons');
+    if (menu.classList.contains('visible')) {
+        icon.innerHTML = "close";
+    } else {
+        icon.innerHTML = "list_alt";
+    }
+};
+
+// Fermer automatiquement le menu mobile lorsqu'un lien est cliqué
+document.querySelectorAll('.header-menu a').forEach(link => {
+    link.addEventListener('click', () => {
+        const menu = document.querySelector('.header-menu');
+        if (menu.classList.contains('visible')) {
+            openMenu();
+        }
+    });
+});
